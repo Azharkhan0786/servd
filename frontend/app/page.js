71bb18811cb05c74f5eb77cce4fee29fd35b8bfd
Azhar Khan <azharkhan844716@gmail.com>
2026-02-02 11:25:23 +0530

    Header section 3348

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>hello </div>
+   <div className="min-h-screen bg-stone-50 text-stone-900">
+    <section className="pt-32 pb-20 px-4">
+      <Button variant="primary" size="xl">Get Started</Button>
+
+    </section>
+   </div>
   );
 }
--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -6,8 +6,8 @@ const Header = () => {
   return (
    <header>
     <SignedOut>
-              <SignInButton />
-              <SignUpButton>
+              <SignInButton  mode='modal'/>
+              <SignUpButton mode='modal'>
                 <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
                   Sign Up
                 </button>
