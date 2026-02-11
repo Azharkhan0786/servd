c3a15160a8552c64790ddb3527377b86f4c7e4c1
Azhar Khan <azharkhan844716@gmail.com>
2026-02-11 09:58:24 +0530

    added custom hook

--- frontend/components/RecipeGrid.jsx
+++ frontend/components/RecipeGrid.jsx
@@ -1,8 +1,8 @@
 export default function RecipeGrid(){
     return(
-       <div className="min-h-screen px-4 pb-16 pt-14">nn
-        <div className="container mx-auto max-w-7xl  ">kn
-            <div className="mb-8">LInk</div>
+       <div className="min-h-screen px-4 pb-16 pt-14">
+        <div className="container mx-auto max-w-7xl  ">
+            <div className="mb-8"></div>
         </div>
        </div>
     )
