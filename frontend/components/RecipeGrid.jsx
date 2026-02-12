33fcadd41edc4f0637fdc7527721bf6f1f7a6d7e
Azhar Khan <azharkhan844716@gmail.com>
2026-02-12 22:28:06 +0530

    added custom hook

--- frontend/components/RecipeGrid.jsx
+++ frontend/components/RecipeGrid.jsx
@@ -1,9 +1,14 @@
-export default function RecipeGrid(){
-    return(
-       <div className="min-h-screen px-4 pb-16 pt-14">
-        <div className="container mx-auto max-w-7xl  ">
-            <div className="mb-8"></div>
-        </div>
-       </div>
-    )
+import React from 'react'
+
+const RecipeGrid = (
+    type,
+    value,
+    fetchAction,
+    backLink="/dashboard"
+) => {
+  return (
+    <div>RecipeGrid</div>
+  )
 }
+
+export default RecipeGrid 
